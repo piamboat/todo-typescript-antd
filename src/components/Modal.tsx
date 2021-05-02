b77f08cbdd53e00450a8ba9580274ffbf888dd42
piamsak <piamsakboat@gmail.com>
2021-05-02 07:47:39 +0700

    ok modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,15 +6,17 @@ interface ModalProps {
 
 const Modal: React.FC<ModalProps> = ({ children, onCancel = () => {} }) => {
   return (
-    <div className='fixed flex items-center py-5 justify-center top-0 right-0 bottom-0 left-0 bg-black bg-opacity-70 z-auto'>
-      <div className='relative bg-gray-200 m-5 p-6 pt-4 md:p-8 md:pt-6 w-96 max-w-full max-h-full overflow-auto'>
-        <button
-          className='absolute text-lg text-gray-600 top-4 right-4 focus:outline-none'
-          onClick={onCancel}
-        >
-          <IoClose />
-        </button>
-        {children}
+    <div className='fixed flex bg-gray-200 items-center py-5 justify-center top-0 right-0 bottom-0 left-0 bg-black bg-opacity-70 min-h-screen z-auto'>
+      <div className='relative m-5 p-6 pt-4 md:p-8 md:pt-6 w-96 max-w-full max-h-full overflow-auto'>
+          <div className="flex">
+            {children}
+            <button
+            className='absolute mb-4 text-lg text-gray-600 focus:outline-none'
+            onClick={onCancel}
+            >
+            <IoClose />
+            </button>
+          </div>
       </div>
     </div>
   );
